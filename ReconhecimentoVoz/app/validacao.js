2ed4538c8741c44f90a83bad4336083727c76098
Vinicius Pires de Oliveira <piresvinicius35@gmail.com>
2023-01-10 23:39:43 -0300

    Adicionando projetos

---
+++ ReconhecimentoVoz/app/validacao.js
@@ -0,0 +1,37 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+    console.log(numero);
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += "<div>Valor inválido</div>"
+        return
+    }
+    if (chuteForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<div>Valor inválido: fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+        setTimeout(function () {
+            window.location.reload()
+        }, 5000)
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function chuteForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
