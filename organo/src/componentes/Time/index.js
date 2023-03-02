aff4b56884dcd16c46f6064e06f236747a7d3698
Vinicius Pires de Oliveira <piresvinicius35@gmail.com>
2023-03-02 12:15:42 -0300

    Subindo projeto

---
+++ organo/src/componentes/Time/index.js
@@ -0,0 +1,20 @@
+import hexToRgba from 'hex-to-rgba';
+import Colaborador from '../Colaborador';
+import './time.css';
+
+const Time = ({ time, colaboradores, aoDeletar, mudarCor }) => {
+    
+    return (
+        colaboradores.length > 0 && <section className='time' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: hexToRgba(time.cor, '0.6') }}>
+            <input onChange={(e) => mudarCor(e.target.value, time.id)} value={time.cor} type='color' className="input-cor" />
+            <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
+            <div className='colaboradores'>
+                {colaboradores.map((colaborador, indice) => {
+                    return <Colaborador key={indice} colaborador={colaborador} corDeFundo={time.cor} aoDeletar={aoDeletar} />
+                })}
+            </div>
+        </section>
+    )
+} 
+
+export default Time
